0fcf5836b6a3db72375032e5f48b6776eeb2de6d
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-12-21 12:24:40 +0300

    feat(CheckConnectInterceptor): create CheckConnectInterceptor
    - создан перехватчик сетевых запросов для проверки наличия интернет-соединения

--- app/src/main/java/ru/zapashnii/weather/di/ApplicationComponent.kt
+++ app/src/main/java/ru/zapashnii/weather/di/ApplicationComponent.kt
@@ -1,6 +1,7 @@
 package ru.zapashnii.weather.di
 
 import dagger.Component
+import ru.zapashnii.weather.network.CheckConnectInterceptor
 import ru.zapashnii.weather.presentation.ui.base_activity.BaseActivity
 import ru.zapashnii.weather.presentation.ui.search_weather.SearchWeatherFragment
 import ru.zapashnii.weather.presentation.ui.start_fragment.StartFragment
@@ -13,4 +14,5 @@ interface ApplicationComponent {
     fun inject(baseActivity: BaseActivity)
     fun inject(startFragment: StartFragment)
     fun inject(searchWeatherFragment: SearchWeatherFragment)
+    fun inject(checkConnectInterceptor: CheckConnectInterceptor)
 }
---
+++ app/src/main/java/ru/zapashnii/weather/network/CheckConnectInterceptor.kt
@@ -0,0 +1,43 @@
+package ru.zapashnii.weather.network
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import ru.zapashnii.weather.R
+import ru.zapashnii.weather.di.MainApp
+import ru.zapashnii.weather.navigation.ViewRouter
+import ru.zapashnii.weather.utils.NetworkUtils
+import ru.zapashnii.weather.utils.Utils
+import javax.inject.Inject
+
+/** Перехватчик сетевых запросов для проверки наличия интернет-соединения */
+class CheckConnectInterceptor : Interceptor {
+
+    @Inject
+    lateinit var viewRouter: ViewRouter
+
+    init {
+        MainApp.instance.applicationComponent.inject(this)
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        if (!NetworkUtils.isInternetAvailable(MainApp.instance.applicationContext)) {
+            viewRouter.singleAlertDialog(Utils.getString(R.string.cannot_connect_to_internet))
+        }
+        return getResponse(chain, chain.request().newBuilder())
+    }
+
+    /**
+     * Сформировать [Response] из [Interceptor.Chain]
+     * @param chain     [Interceptor.Chain]
+     * @param builder   [Request.Builder]
+     * @return          [Response]
+     */
+    private fun getResponse(chain: Interceptor.Chain, builder: Request.Builder): Response {
+        return try {
+            chain.proceed(builder.build())
+        } catch (ex: Exception) {
+            throw ex
+        }
+    }
+}
