1221ade08aa86532492d45a18ed36dce7f5d1100
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-12-23 17:51:10 +0300

    doc(Weather): add doc
    - Добавлена документация

--- app/src/main/java/ru/zapashnii/weather/domain/model/Weather.kt
+++ app/src/main/java/ru/zapashnii/weather/domain/model/Weather.kt
@@ -2,6 +2,23 @@ package ru.zapashnii.weather.domain.model
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * TODO
+ *
+ * @property base
+ * @property clouds         Облачность
+ * @property cod
+ * @property coord          Координаты города
+ * @property dt             Время расчета данных, unix, UTC
+ * @property id             ID города
+ * @property main
+ * @property name           Название города
+ * @property sys
+ * @property timezone       Сдвиг в секундах от UTC
+ * @property visibility
+ * @property weather
+ * @property wind
+ */
 data class Weather(
     @SerializedName("base")
     val base: String,
@@ -31,11 +48,22 @@ data class Weather(
     val wind: Wind,
 )
 
+/**
+ * Облачность
+ *
+ * @property all    Облачность,%
+ */
 data class Clouds(
     @SerializedName("all")
     val all: Int,
 )
 
+/**
+ * Координаты города
+ *
+ * @property lat    Географическое положение города, долгота
+ * @property lon    Географическое положение города, широта
+ */
 data class Coord(
     @SerializedName("lat")
     val lat: Double,
@@ -43,6 +71,16 @@ data class Coord(
     val lon: Double,
 )
 
+/**
+ * TODO
+ *
+ * @property feels_like     Температура. Этот температурный параметр объясняет человеческое восприятие погоды.
+ * @property humidity       Влажность, %
+ * @property pressure       Атмосферное давление (на уровне моря, если нет данных sea_level или grnd_level), гПа
+ * @property temp           Температура
+ * @property temp_max       Максимальная температура на данный момент
+ * @property temp_min       Минимальная температура на данный момент
+ */
 data class Main(
     @SerializedName("feels_like")
     val feels_like: Double,
@@ -58,6 +96,16 @@ data class Main(
     val temp_min: Double,
 )
 
+/**
+ * TODO
+ *
+ * @property country    Код страны
+ * @property id
+ * @property message
+ * @property sunrise
+ * @property sunset
+ * @property type
+ */
 data class Sys(
     @SerializedName("country")
     val country: String,
@@ -73,6 +121,14 @@ data class Sys(
     val type: Int,
 )
 
+/**
+ * TODO
+ *
+ * @property description
+ * @property icon
+ * @property id
+ * @property main
+ */
 data class WeatherX(
     @SerializedName("description")
     val description: String,
@@ -84,9 +140,18 @@ data class WeatherX(
     val main: String,
 )
 
+/**
+ * TODO
+ *
+ * @property deg    Направление ветра, градусы (метеорологические)
+ * @property speed  Скорость ветра
+ * @property gust   Порыв ветра
+ */
 data class Wind(
     @SerializedName("deg")
     val deg: Int,
     @SerializedName("speed")
     val speed: Double,
+    @SerializedName("gust")
+    val gust: Double,
 )
